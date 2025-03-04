393196c2bf4bb22a3a5007c23f0911b3c502d514
unknown <ifumin.hs@gmail.com>
2025-03-04 15:22:14 -0500

    Change Topic adding type

--- action/admin.py
+++ action/admin.py
@@ -8,6 +8,8 @@ class TopicAdmin(admin.ModelAdmin):
 
 @admin.register(Action)
 class ActionAdmin(admin.ModelAdmin):
-    list_display = ("date", "name_of_action", "description", "source")  # 顯示主要欄位
-    search_fields = ("name_of_action", "description", "meaning")  # 支援搜尋
-    list_filter = ("topics", "date")  # 提供過濾器
+    list_display = ('name_of_action', 'date', 'description')
+    filter_horizontal = ('topics',)  # ✅ 讓 Topic 變成可勾選選項
+
+admin.site.register(Action, ActionAdmin)
+admin.site.register(Topic)
